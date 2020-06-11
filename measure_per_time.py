cfce3a11b34c405641b64c5883339eb09e123537
wooruang <wooruang@gmail.com>
2020-06-11 15:23:47 +0900

    Modify 'measure_per_time.py'

--- measure_per_time.py
+++ measure_per_time.py
@@ -4,105 +4,172 @@ import numpy as np
 import time
 import sys
 
-maximum = 0
+
+maximum_sec = 0
 seconds = [1]
 labels = ['Occur1']
-estimation_count = [1]
-interval_sec = [1]
+measure_counts = [1]
+alarm_interval_seconds = [1]
 
-last_estimated_times = []
 input_times = []
+states = []
+next_alarm_times = []
+
+cache_input = None
+
+PROPS_NAME_SECONDS = "seconds"
+PROPS_NAME_LABELS = "labels"
+PROPS_NAME_MEASURE_COUNT = "measure_counts"
+PROPS_NAME_ALARM_INTERVAL_SECONDS = "alarm_interval_seconds"
 
 
 def on_set(k, v):
-    if k == "seconds":
+    if k == PROPS_NAME_SECONDS:
         global seconds
-        global longest
+        global maximum_sec
         seconds = [int(i) for i in v.split(',')]
-        maximum = max(seconds)
-    elif k == "labels":
+        maximum_sec = max(seconds)
+    elif k == PROPS_NAME_LABELS:
         global labels
         labels = v.split(',')
-    elif k == "estimation_count":
-        global estimation_count
-        estimation_count = [int(i) for i in v.split(',')]
-    elif k == "":
-        global estimation_count
-        estimation_count = [int(i) for i in v.split(',')]
+    elif k == PROPS_NAME_MEASURE_COUNT:
+        global measure_counts
+        measure_counts = [int(i) for i in v.split(',')]
+    elif k == PROPS_NAME_ALARM_INTERVAL_SECONDS:
+        global alarm_interval_seconds
+        alarm_interval_seconds = [int(i) for i in v.split(',')]
 
 
 def on_get(k):
-    if k == "seconds":
+    if k == PROPS_NAME_SECONDS:
         s = [str(i) for i in seconds]
         return ','.join(s)
-    elif k == "labels":
+    elif k == PROPS_NAME_LABELS:
         return ','.join(labels)
-    elif k == "estimation_count":
-        return str(estimation_count)
+    elif k == PROPS_NAME_MEASURE_COUNT:
+        c = [str(i) for i in measure_counts]
+        return ','.join(c)
+    elif k == PROPS_NAME_ALARM_INTERVAL_SECONDS:
+        c = [str(i) for i in alarm_interval_seconds]
+        return ','.join(c)
 
 
-def put_new_time(t):
-    global input_times
-    input_times.append(t)
-
-
-def estimate_in_time(cur_t):
-
-    estimated = [[]] * len(seconds)
-
-    # 전체
-    for t in input_times:
-        dt = cur_t - t
-
-        for idx, s in enumerate(seconds):
-            if s <= dt:
-                estimated[idx].append(t)
-
-    return estimated
-
-
-def estimate_out(cur_t, estimated):
-    global last_estimated_times
-
-    result_labels = [None] * len(seconds)
-
-    dtimes = [cur_t - i for i in last_estimated_times]
-
-    for idx, e in enumerate(estimated):
-        if dtimes[idx] < interval_sec[idx]:
-            continue
-        else:
-            if len(e) >= estimation_count[idx]:
-                last_estimated_times[idx] = cur_t
-                result_labels[idx] = labels[idx]
-
-    return result_labels
+def on_init():
+    return initialize_variables()
 
 
 def on_run(array):
 
-    # cur_time.
+    # current_time.
     cur_t = time.time()
 
-    # Input new one.
-    put_new_time(cur_t)
+    if not check_valid_props():
+        sys.stderr.write(
+            "[measure_per_time.on_run] states is empty. Because props is invalid value.\n")
+        return
 
-    # Estimate in time.
-    estimated = estimate_in_time(cur_t)
+    remove_expired(cur_t)
 
-    # Estimate result.
-    result_labels = estimate_out(cur_t, estimated)
+    if array.size != 0:
+        update_cache(array)
+        add_new_time(cur_t)
 
-    # Filter
-    result_labels = [l for l in result_labels if l is not None]
+    measure_state(cur_t)
 
-    condition_result = 0 if len(result_labels) == 0 else 1
+    alarms = measure_alarm(cur_t)
+    if not alarms:
+        return
 
     return {
-        'condition': np.array([condition_result], np.int32),
-        'labels': np.array()
+        'result': cache_input,
+        'labels': np.array(alarms)
     }
 
 
 def on_destroy():
     return True
+
+
+def initialize_variables():
+    global states
+    global next_alarm_times
+    if not check_valid_props():
+        return False
+    states = [False] * len(seconds)
+    next_alarm_times = [0] * len(seconds)
+    return True
+
+
+def check_valid_props():
+    return len(seconds) == len(labels) == len(measure_counts)
+
+
+def remove_expired(current):
+    global input_times
+    input_times = list(
+        filter(lambda x: current - x < maximum_sec, input_times))
+
+
+def add_new_time(t):
+    global input_times
+    input_times.append(t)
+
+
+def update_cache(v):
+    global cache_input
+    cache_input = v
+
+
+def reset_cache():
+    global cache_input
+    cache_input = None
+
+
+def is_active(current, sec, count):
+    filtered = list(filter(lambda x: current - x <= sec, input_times))
+    return len(filtered) >= count
+
+
+def active(idx):
+    states[idx] = True
+
+
+def deactive(idx):
+    states[idx] = False
+    next_alarm_times[idx] = 0
+    reset_cache()
+
+
+def measure_state(cur_t):
+
+    if not input_times:
+        return False
+
+    for idx, items in enumerate(zip(seconds, measure_counts)):
+        sec, mcount = items
+
+        if is_active(cur_t, sec, mcount):
+            active(idx)
+        else:
+            deactive(idx)
+
+
+def measure_alarm(current):
+    alarm_idxes = []
+    for idx, items in enumerate(zip(states, alarm_interval_seconds, next_alarm_times)):
+        state, interval, next_time = items
+
+        if not state:
+            continue
+
+        if next_time == 0:
+            next_alarm_times[idx] = current
+            alarm_idxes.append(idx)
+            continue
+
+        dt = current - next_time
+
+        if dt >= interval:
+            alarm_idxes.append(idx)
+            next_alarm_times[idx] += interval
+    return alarm_idxes
--- measure_per_time_test.py
+++ measure_per_time_test.py
@@ -1,13 +1,30 @@
+import numpy as np
 import cv2
+import measure_per_time as mpt
+
 
 def main():
+
+    mpt.on_set(mpt.PROPS_NAME_SECONDS, "5")
+    mpt.on_set(mpt.PROPS_NAME_LABELS, "No Helmet!")
+    mpt.on_set(mpt.PROPS_NAME_MEASURE_COUNT, "5")
+    mpt.on_set(mpt.PROPS_NAME_ALARM_INTERVAL_SECONDS, "3")
+
+    if not mpt.on_init():
+        print("Initialize Fail!")
+        return
 
     while True:
         empty = np.zeros((500, 500, 3), np.int8)
 
-        fps = get_fps()
         empty = cv2.putText(
-            empty, str(fps), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            empty, str(mpt.states), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        empty = cv2.putText(
+            empty, str(mpt.next_alarm_times), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        empty = cv2.putText(
+            empty, str(mpt.cache_input), (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow("image", empty)
         key = cv2.waitKey(1)
@@ -15,7 +32,15 @@ def main():
         if key == 27:
             break
         elif key == ord('1'):
-            on_run(['1'])
+            mpt.on_run(np.array([1]))
+        elif key == ord('2'):
+            mpt.on_run(np.array([2]))
+        elif key == ord('3'):
+            mpt.on_run(np.array([3]))
+        elif key == ord('4'):
+            mpt.on_run(np.array([4]))
+        else:
+            mpt.on_run(np.array([]))
 
 
 if __name__ == "__main__":
